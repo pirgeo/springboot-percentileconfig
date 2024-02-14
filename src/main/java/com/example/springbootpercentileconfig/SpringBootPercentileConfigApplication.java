ff734918467dc847702c9197c31ac60a662acbac
Georg P <georg.pirklbauer@dynatrace.com>
2024-02-14 10:03:44 +0100

    initial commit

---
+++ src/main/java/com/example/springbootpercentileconfig/SpringBootPercentileConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpercentileconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPercentileConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPercentileConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootpercentileconfig/SpringBootPercentileConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpercentileconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPercentileConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
